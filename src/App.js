a2069d64690cbb61158d7beffd41f2583d98db2b
Sam Thompson <samthompson292@gmail.com>
2021-06-10 10:40:33 +0100

    changed structure and colours

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import * as THREE from 'three' 
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { EffectComposer, ChromaticAberration, DepthOfField } from '@react-three/postprocessing'
-import { Html } from '@react-three/drei'
-import clamp from 'clamp'
+import React, { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { EffectComposer, ChromaticAberration, DepthOfField, Bloom } from '@react-three/postprocessing'
+import { Loader } from './Loader'
+import { Shape } from './Shape'
 import "./ColorMaterial"
 import { 
     Button, 
     Typography, 
     Switch,
-    Grid,
     Paper, 
     ThemeProvider, 
     createMuiTheme, 
@@ -23,82 +21,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-const tempObject = new THREE.Object3D()
-
-
-
-const Shape = ({ count, paletteNumber }) => {
-
-  const colors = require('nice-color-palettes');
-
-  const easeInOutQuint = function(pos) {
-  if ((pos/=0.5) < 1) return 0.5*Math.pow(pos, 5);
-  return 0.5 * (Math.pow((pos-2),5) + 2);
-  }
-
-  let palette = colors[paletteNumber]
-
-  const mesh = useRef()
-
-  const [hovered, setHovered] = useState(false)
-
-  useFrame((state) => {
-    const mouse = state.mouse
-    let time = state.clock.getElapsedTime()%5
-    let playhead = time/5
-    let i = 0
-    let number = count
-
-    mesh.current.rotation.x = mouse.y * 0.02
-    mesh.current.rotation.y = mouse.x * 0.02
-
-    for (let x = 0; x <= number; x++)
-      for (let z = 0; z <= number; z++) {
-        let offset = Math.sqrt( (x/number - 0.5) **2 + (z/number - 0.5) **2 )/Math.sqrt(0.5**2 + 0.5**2)
-        const id = i++
-        let progress = easeInOutQuint(clamp((playhead - 0.4 * offset) /0.6, 0, 1))
-        let sine = Math.sin(progress*Math.PI)
-        const scale = 1
-        const hover = id === hovered ? 1.4 : 1
-
-        tempObject.position.set(
-            30*(x/number - 0.5) + (mouse.y * 0.1), 
-            offset * 1 * hover, 
-            30*(z/number - 0.5) + (mouse.x * 0.1)
-        )
-        tempObject.rotation.y = progress*Math.PI
-        tempObject.scale.set(scale + 0.2*sine, scale + 0.1*sine, scale + 0.2*sine)
-        tempObject.updateMatrix()
-        mesh.current.setMatrixAt(id, tempObject.matrix)
-      }
-
-    mesh.current.instanceMatrix.needsUpdate = true
-  })
-
-  return (
-    <instancedMesh
-      ref={mesh}
-      args={[null, null, 1000]}
-      onPointerMove={(e) => setHovered(e.instanceId)}
-      onPointerOut={(e) => setHovered(undefined)}
-      >
-      <boxBufferGeometry args={[0.9, 10, 0.9]} />
-      <colorMaterial 
-        color={palette[1]} 
-        color1={palette[2]} 
-        color2={palette[3]} 
-      />
-    </instancedMesh>
-  )
-}
-
-
-
 function App() {
 
     const classes = useStyles();
     const [darkMode, setDarkMode] = useState(false)
-    const [paletteNumber, setPaletteNumber] = useState(darkMode ? 94 : 96)
+    const [paletteNumber, setPaletteNumber] = useState(darkMode ? 36 : 11)
+
+    console.log(paletteNumber)
 
     let theme = createMuiTheme({
     palette: {
@@ -135,41 +64,38 @@ function App() {
             onCreated={({gl}) => {
                 gl.setClearColor(paletteNumber[4])
             }}
-            colorManagement
+            colorManagement={true}
             camera={{
-                position: [-6, 7, -3]
+                // position: [-6, 7, -3],
+                position: [-10.8, 7.6, -6],
+                fov: 50,
+                near: 1, 
             }}>
                 <ambientLight intensity={0.5}/>
                 <Shape position={[0, 0, 0]} count={20} paletteNumber={paletteNumber}/>
                 <EffectComposer>
                     <DepthOfField
                         target={[-6, 7, -3]}
-                        focalLength={0.03}
+                        focalLength={0.02}
                         bokehScale={10}
                         height={500}
                     />
-                    <ChromaticAberration offset={[-0.001, 0.004]} />
+                    <ChromaticAberration offset={[-0.001, 0.002]} />
+                    <Bloom luminanceThreshold={0.3} luminanceSmoothing={0.9} height={100} />
                 </EffectComposer>
             </Canvas>
+            <Loader />
             <ThemeProvider theme={theme}>
                 <Paper>
-                    <Grid className={classes.header}>
                         <Switch 
                             checked={darkMode} 
                             onChange={() => {
                                 setDarkMode(!darkMode) 
-                                setPaletteNumber(darkMode ? 96 : 94)
+                                setPaletteNumber(darkMode ? 11 : 36)
                             }} 
                         />
-                    </Grid>
-                    <Grid className={classes.heroTextWrapper}>
                         <Typography variant="h1" className={classes.heroText} color="secondary">Full stack developer</Typography>
-                    </Grid>
-                    <div>
-                        <Typography>This is my portfolio</Typography>
                         <Button variant="contained" color="primary" size="large" onClick={() => setPaletteNumber(Math.floor(Math.random() * 100) + 1)}>New color</Button>
-
-                    </div>
                 </Paper>
             </ThemeProvider>
         </>
---
+++ src/Shape.js
@@ -0,0 +1,74 @@
+import * as THREE from 'three' 
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import clamp from 'clamp'
+import "./ColorMaterial"
+
+
+const tempObject = new THREE.Object3D()
+
+
+export const Shape = ({ count, paletteNumber }) => {
+
+  const colors = require('nice-color-palettes');
+
+  const easeInOutQuint = function(pos) {
+  if ((pos/=0.5) < 1) return 0.5*Math.pow(pos, 5);
+  return 0.5 * (Math.pow((pos-2),5) + 2);
+  }
+
+  let palette = colors[paletteNumber]
+
+  const mesh = useRef()
+
+  const [hovered, setHovered] = useState(false)
+
+  useFrame((state) => {
+    const mouse = state.mouse
+    let time = state.clock.getElapsedTime()%5
+    let playhead = time/5
+    let i = 0
+    let number = count
+
+    mesh.current.rotation.x = mouse.y * 0.02
+    mesh.current.rotation.y = mouse.x * 0.02
+
+    for (let x = 0; x <= number; x++)
+      for (let z = 0; z <= number; z++) {
+        let offset = Math.sqrt( (x/number - 0.5) **2 + (z/number - 0.5) **2 )/Math.sqrt(0.5**2 + 0.5**2)
+        const id = i++
+        let progress = easeInOutQuint(clamp((playhead - 0.4 * offset) /0.6, 0, 1))
+        let sine = Math.sin(progress*Math.PI)
+        const scale = 1
+        const hover = id === hovered ? 1.4 : 1
+
+        tempObject.position.set(
+            30*(x/number - 0.5) + (mouse.y * 0.1), 
+            offset * 1 * hover, 
+            30*(z/number - 0.5) + (mouse.x * 0.1)
+        )
+        tempObject.rotation.y = progress*Math.PI
+        tempObject.scale.set(scale + 0.2*sine, scale + 0.1*sine, scale + 0.2*sine)
+        tempObject.updateMatrix()
+        mesh.current.setMatrixAt(id, tempObject.matrix)
+      }
+
+    mesh.current.instanceMatrix.needsUpdate = true
+  })
+
+  return (
+    <instancedMesh
+      ref={mesh}
+      args={[null, null, 1000]}
+      onPointerMove={(e) => setHovered(e.instanceId)}
+      onPointerOut={(e) => setHovered(undefined)}
+      >
+      <boxBufferGeometry args={[0.9, 10, 0.9]} />
+      <colorMaterial 
+        color={palette[1]} 
+        color1={palette[2]} 
+        color2={palette[3]} 
+      />
+    </instancedMesh>
+  )
+}
