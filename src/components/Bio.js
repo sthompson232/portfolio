cd15d6db106e941f6b3b00cdaf702d0319559336
Sam Thompson <samthompson292@gmail.com>
2021-06-14 15:47:22 +0100

    animation to portfolio
    just need to animate text

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Typography, Box, Grid, Container, Link } from '@material-ui/core'
 import { useStyles } from '../styles'
 import bio from '../static/bio.jpeg'
@@ -7,11 +7,49 @@ import EmailIcon from '@material-ui/icons/Email'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import cv from '../static/cv.pdf'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 
 export const Bio = ({ background, color, color2, revColor, revButton }) => {
     const classes = useStyles();
-      
+    const titleRef = useRef(null)
+    const imageRef = useRef(null)
+
+    useEffect(() => {
+        gsap.fromTo(titleRef.current, {
+            x: -100,
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            duration: 2,
+            x: 0,
+            scrollTrigger: {
+                trigger: titleRef.current,
+                start: 'top bottom',
+                toggleActions: 'play none none reverse',
+            }
+        })
+    }, [titleRef])
+
+    useEffect(() => {
+        gsap.fromTo(imageRef.current, {
+            x: 100,
+            autoAlpha: 0
+        }, {
+            x: 0,
+            autoAlpha: 1,
+            duration: 2,
+            scrollTrigger: {
+                trigger: titleRef.current,
+                start: 'top bottom',
+                toggleActions: 'play none none reverse',
+            }
+        })
+    }, [imageRef])
+       
     return (
         <Box style={{ backgroundColor: background, border: 'none' }}>
             <Box py={8}>
@@ -36,7 +74,7 @@ export const Bio = ({ background, color, color2, revColor, revButton }) => {
 
                     </Grid>
                     <Grid container item justify='center' sm={12} lg={5} xl={4} xxl={2}>
-                        <img className={classes.bioImage} src={bio} alt="Sam Thompson"/>
+                        <img style={{ overflow: 'hidden' }} ref={imageRef} className={classes.bioImage} src={bio} alt="Sam Thompson"/>
                     </Grid>
                 </Grid>
             </Box>
@@ -44,13 +82,15 @@ export const Bio = ({ background, color, color2, revColor, revButton }) => {
                 <Grid container>
                     <Grid container item xs={12}>
                         <Box p={4}>
-                            <Typography
-                                variant="h1" 
-                                className={classes.headingText}
-                                style={{ color: color2 }}
-                                >
-                                Connect with me
-                            </Typography>
+                            <div ref={titleRef}>
+                                <Typography
+                                    variant="h1" 
+                                    className={classes.headingText}
+                                    style={{ color: color2 }}
+                                    >
+                                    Connect with me
+                                </Typography>
+                            </div>
                         </Box>
                     </Grid>
                     <Container maxWidth={false}>
--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Grid, Paper, Link } from '@material-ui/core'
 import { useStyles } from '../styles'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+gsap.registerPlugin(ScrollTrigger)
 
 export const ContactButton = ({ icon, button, link }) => {
     const classes = useStyles();
+    const buttonRef = useRef(null)
+
+    useEffect(() => {
+        gsap.fromTo(buttonRef.current, {
+            autoAlpha: 0,
+        }, {
+            duration: 1, 
+            autoAlpha: 1, 
+            ease: 'none',
+            scrollTrigger: {
+                trigger: buttonRef.current,
+                start: 'top bottom',
+                end: '-=300',
+                toggleActions: 'play none none reverse',
+            }
+        })
+    }, [buttonRef])
 
     return (
         <Grid item xs={12} md={4}>
-            <Link href={link} target="_blank">
-                <Paper className={`${classes.button} hover`} style={{ backgroundColor: button }}>
-                    {icon}
-                </Paper>
-            </Link>
+            <div ref={buttonRef}>
+                <Link href={link} target="_blank">
+                    <Paper className={`${classes.button} hover`} style={{ backgroundColor: button }}>
+                        {icon}
+                    </Paper>
+                </Link>
+            </div>
         </Grid>
     )
 }
