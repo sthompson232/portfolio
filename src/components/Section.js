866f9d45b23f5c8ac262d5990265c7eafc5d4b33
Sam Thompson <samthompson292@gmail.com>
2021-06-14 11:25:02 +0100

    added gsap to section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Typography, Box } from '@material-ui/core'
 import { useStyles } from '../styles'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+gsap.registerPlugin(ScrollTrigger)
 
 export const Section = ({background, color, content}) => {
+    const textRef = useRef(null)
     const classes = useStyles();
+
+    useEffect(() => {
+
+        gsap.fromTo(textRef.current, {
+            autoAlpha: 0
+        }, {
+            duration: 1.5, 
+            autoAlpha: 1, 
+            ease: 'none',
+            scrollTrigger: {
+                trigger: textRef.current,
+                start: 'top bottom-=200',
+                end: 'bottom top',
+                toggleActions: 'play none none reverse',
+                markers: true
+            }
+        })
+
+    }, [textRef])
 
     return (
         <Box
             py={20}
             style={{ backgroundColor: background, border: 'none' }}
         >
-            <Typography
-                variant="h1" 
-                className={classes.headingText}
-                style={{ color: color }}
-                >
-                {content}
-            </Typography>
+            <div ref={textRef}>
+                <Typography
+                    variant="h1" 
+                    className={classes.headingText}
+                    style={{ color: color }}
+                    >
+                    {content}
+                </Typography>
+            </div>
         </Box>
     )
 }
