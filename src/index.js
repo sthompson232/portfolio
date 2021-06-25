2265ab7f8dfebb3650b05a7942d326485c396823
Sam Thompson <samthompson292@gmail.com>
2021-06-25 19:25:36 +0100

    added react-ga

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.scss'
 import ReactDOM from 'react-dom';
 import { Grid } from '@material-ui/core'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('G-EW6V9K4745');
 const App = React.lazy(() => import ('./App'))
 
 ReactDOM.render(
